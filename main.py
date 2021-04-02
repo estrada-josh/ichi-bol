c56e86d4f67c9b484c2ddbb85e56355c1d2f5a2c
Joshua Estrada <estrada.josh@gmail.com>
2021-04-02 13:09:56 -0400

    trying to get this on a web app

--- main.py
+++ main.py
@@ -10,50 +10,50 @@ from statistics import mean
 import funcs
 from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def index():
+#     return
 
-@app.route('/')
-def index():
-    return
+tickers = []
+alerts_list = []
+# files = ['test.csv']
+files = ['amex.csv','nasdaq.csv','nyse.csv']
 
-    tickers = []
-    alerts_list = []
-    # files = ['test.csv']
-    files = ['amex.csv','nasdaq.csv','nyse.csv']
+for file in files:
+    tick_list = funcs.filters(file)
+    for x in tick_list:
+        tickers.append(x)
 
-    for file in files:
-        tick_list = funcs.filters(file)
-        for x in tick_list:
-            tickers.append(x)
+for ticker in tickers:
 
-    for ticker in tickers:
+    df = funcs.data(ticker)
+    score = funcs.scores(df)
+    alerts = funcs.alerts(score)
+    sim = funcs.sim(score)
 
-        df = funcs.data(ticker)
-        score = funcs.scores(df)
-        alerts = funcs.alerts(score)
-        sim = funcs.sim(score)
+    simlen = len(sim)
+    simtot = 0
+    for i in sim:
+        simtot = i + simtot
 
-        simlen = len(sim)
-        simtot = 0
-        for i in sim:
-            simtot = i + simtot
+    avg = round((simtot / simlen),2)
 
-        avg = round((simtot / simlen),2)
+    if avg >= 10:
+        print()
+        print(ticker)
+        print(score.tail(3))
+        print()
+        print('Trade Sim Results (%): ',sim)
+        print('Average return %: ',avg)
+        print()
+        if len(alerts) > 0:
+            alerts_list.append(alerts)
 
-        if avg >= 10:
-            print()
-            print(ticker)
-            print(score.tail(3))
-            print()
-            print('Trade Sim Results (%): ',sim)
-            print('Average return %: ',avg)
-            print()
-            if len(alerts) > 0:
-                alerts_list.append(alerts)
+print('### ALERTS ###\n')
+for alert in alerts_list:
+    print(alert)
 
-    print('### ALERTS ###\n')
-    for alert in alerts_list:
-        print(alert)
-
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
