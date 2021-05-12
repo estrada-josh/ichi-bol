08c416f4e727a5989555c1a1019dea4f4b0e02f7
Joshua Estrada <estrada.josh@gmail.com>
2021-05-12 13:05:51 -0400

    Added comments for understanding

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Main program that runs the analysis
+
 import pandas as pd
 from datetime import date
 import numpy as np
@@ -9,31 +11,48 @@ import requests
 from statistics import mean
 import funcs
 
+# Lists to be populated by later operations
 tickers = []
 alerts_list = []
+
 # files = ['test.csv']
 files = ['amex.csv','nasdaq.csv','nyse.csv']
 
+# Looks through the files listed in 'files'
+# 'filters' function returns a narrowed down list of tickers
+#  the tickers are then added to the empty list created above
 for file in files:
     tick_list = funcs.filters(file)
     for x in tick_list:
         tickers.append(x)
 
+# Runs list of tickers through each function
 for ticker in tickers:
 
+    # Pulls price data from the web and creates dataframe
     df = funcs.data(ticker)
+
+    # scores each tickers current performance
     score = funcs.scores(df)
+
+    # decides when to buy or sell based on performance
     alerts = funcs.alerts(score)
+
+    # simulates buying and selling for the past 60 days based on performance score
+    # returns a list of buy and sell results
     sim = funcs.sim(score)
 
+    # Calculate average results
     simlen = len(sim)
     simtot = 0
     for i in sim:
         simtot = i + simtot
 
+    # Only shows tickers that had more than one buy or sell
     if simlen > 0 and simtot > 0:
         avg = round((simtot / simlen),2)
 
+        # prints results for ticker
         if avg >= 5:
             print()
             print(ticker)
@@ -45,6 +64,7 @@ for ticker in tickers:
             if len(alerts) > 0:
                 alerts_list.append(alerts)
 
+# Prints every buy and sell alert for the current day
 print('### ALERTS ###\n')
 for alert in alerts_list:
     print(alert)
